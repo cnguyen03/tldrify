c013fe5a88c629d8a81e5daf97bb1f123642bcbd
Calvin Nguyen <calvinnguyen03@gmail.com>
2023-12-27 17:04:30 -0800

    initial abstraction summarization implementation

--- server/server.py
+++ server/server.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests 
 from bs4 import BeautifulSoup
-from openai import OpenAI
 
 from nltk.tokenize import word_tokenize
+
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
 
 # app instance
 app = Flask(__name__)
@@ -13,8 +17,8 @@ CORS(app)
 # displaying it on frontend
 
 # OpenAI
-my_key = open("API_KEY.txt", "r").read()
-client = OpenAI(api_key=my_key)
+# my_key = open("API_KEY.txt", "r").read()
+# client = OpenAI(api_key=my_key)
 
 # Steps to get app running 
 # 1. Open new terminal, go to client directory and run source ~/.bashrc
@@ -29,33 +33,69 @@ client = OpenAI(api_key=my_key)
 @app.route("/api/home", methods=['GET', 'POST'])
 def handle_request():
     # Code to process data sent from the front end
-    message = ""
-    content = ""
     data = request.get_json()
     link = data.get("link")
-    # Send an html request to the link
-    response = requests.get(link)
+    # Parse page for text
+    text = parse(link)
+    # Additional processing of message
+    # sentences, words = process_text(text)
+    # Get summary
+    summary = summarize(text, 0.05)
+
+    # Send message to OpenAI API
+    # completion = client.chat.completions.create(
+    # model="gpt-3.5-turbo",
+    # messages=[
+    #         {"role": "user", "content": f"Please give me a TLDR of the following text: {message}"}
+    #     ]
+    # )
+    # content = completion.choices[0].message.content
+
+    # Code to send a response to the front end
+    response = {"message": summary}
+    return jsonify(response)
+def parse(url):
+    response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Parse the text and add it to message
+        # parse the text and add it to message
         all_text = soup.get_text()
+        # tokenize text to words
         words = word_tokenize(all_text)
         message = ' '.join(words)
-        # Additional Processing of Message
+        return message
+    else: 
+        return "Invalid URL"
 
-        # Send message to OpenAI API
-        completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "user", "content": f"Please give me a TLDR of the following text: {message}"}
-            ]
-        )
-        content = completion.choices[0].message.content
-    else:
-        message = f"Failed to retrieve the page. Status code: {response.status_code}"
-    # Code to send a response to the front end
-    response = {"message": content}
-    return jsonify(response)
+def summarize(text, per):
+    nlp = spacy.load('en_core_web_sm')
+    doc= nlp(text)
+    tokens=[token.text for token in doc]
+    word_frequencies={}
+    for word in doc:
+        if word.text.lower() not in list(STOP_WORDS):
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    max_frequency=max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word]=word_frequencies[word]/max_frequency
+    sentence_tokens= [sent for sent in doc.sents]
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():                            
+                    sentence_scores[sent]=word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent]+=word_frequencies[word.text.lower()]
+    select_length=int(len(sentence_tokens)*per)
+    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
+    final_summary=[word.text for word in summary]
+    summary=''.join(final_summary)
+    return summary
 
 
 if __name__ == "__main__":
