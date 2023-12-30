2787933382000cbf81376deabd0eeea3694248f3
Calvin Nguyen <97204336+cnguyen03@users.noreply.github.com>
2023-12-29 22:53:01 -0800

    ai pipeline for summaries

--- server/server.py
+++ server/server.py
@@ -5,20 +5,15 @@ from bs4 import BeautifulSoup
 
 from nltk.tokenize import word_tokenize
 
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from heapq import nlargest
-
+from transformers import pipeline
 # app instance
 app = Flask(__name__)
 CORS(app)
 # use next.js frontend to fetch flask backend route and grab data sent,
 # displaying it on frontend
 
-# OpenAI
-# my_key = open("API_KEY.txt", "r").read()
-# client = OpenAI(api_key=my_key)
+# AI Pipeline
+summarizer = pipeline("summarization")
 
 # Steps to get app running 
 # 1. Open new terminal, go to client directory and run source ~/.bashrc
@@ -36,66 +31,49 @@ def handle_request():
     data = request.get_json()
     link = data.get("link")
     # Parse page for text
-    text = parse(link)
-    # Additional processing of message
-    # sentences, words = process_text(text)
-    # Get summary
-    summary = summarize(text, 0.05)
+    text = parse(link) 
 
-    # Send message to OpenAI API
-    # completion = client.chat.completions.create(
-    # model="gpt-3.5-turbo",
-    # messages=[
-    #         {"role": "user", "content": f"Please give me a TLDR of the following text: {message}"}
-    #     ]
-    # )
-    # content = completion.choices[0].message.content
+    max_chunk = 900
+    text = text.replace('.', '.<eos>')
+    text = text.replace('?', '?<eos>')
+    text = text.replace('!', '!<eos>')
 
-    # Code to send a response to the front end
-    response = {"message": summary}
+    sentences = text.split('<eos>')
+    current_chunk = 0 
+    chunks = []
+    for sentence in sentences:
+        if len(chunks) == current_chunk + 1: 
+            if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
+                chunks[current_chunk].extend(sentence.split(' '))
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(' '))
+        else:
+            print(current_chunk)
+            chunks.append(sentence.split(' '))
+
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = ' '.join(chunks[chunk_id])
+
+    res = summarizer(chunks, max_length=500, min_length=30, do_sample=False)
+    text = ' '.join([summ['summary_text'] for summ in res])
+
+    response = {"message": text}
     return jsonify(response)
+
 def parse(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         # parse the text and add it to message
-        all_text = soup.get_text()
+        paragraphs = soup.find_all(['p', 'h1'])
+        all_text = [paragraph.text for paragraph in paragraphs]
         # tokenize text to words
-        words = word_tokenize(all_text)
+        words = word_tokenize(' '.join(all_text))
         message = ' '.join(words)
         return message
     else: 
         return "Invalid URL"
-
-def summarize(text, per):
-    nlp = spacy.load('en_core_web_sm')
-    doc= nlp(text)
-    tokens=[token.text for token in doc]
-    word_frequencies={}
-    for word in doc:
-        if word.text.lower() not in list(STOP_WORDS):
-            if word.text.lower() not in punctuation:
-                if word.text not in word_frequencies.keys():
-                    word_frequencies[word.text] = 1
-                else:
-                    word_frequencies[word.text] += 1
-    max_frequency=max(word_frequencies.values())
-    for word in word_frequencies.keys():
-        word_frequencies[word]=word_frequencies[word]/max_frequency
-    sentence_tokens= [sent for sent in doc.sents]
-    sentence_scores = {}
-    for sent in sentence_tokens:
-        for word in sent:
-            if word.text.lower() in word_frequencies.keys():
-                if sent not in sentence_scores.keys():                            
-                    sentence_scores[sent]=word_frequencies[word.text.lower()]
-                else:
-                    sentence_scores[sent]+=word_frequencies[word.text.lower()]
-    select_length=int(len(sentence_tokens)*per)
-    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
-    final_summary=[word.text for word in summary]
-    summary=''.join(final_summary)
-    return summary
 
 
 if __name__ == "__main__":
